8c54620e33fb86026c564724b02c5c1896c82b3e
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-06-08 17:33:01 +0200

    update

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Unitech</h1>
+    <div className="w-full h-screen bg-black grid justify-center text-center items-center">
+      <h1 className="text-white text-7xl">Unitech</h1>
     </div>
   );
 }
