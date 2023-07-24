420e2c345a49990ac89b8c3f210ceca22371fe69
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-07-24 23:54:06 +0200

    update

---
+++ src/app/components/landing/index.client.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Landng = () => {
+  return (
+    <div className="w-full h-screen bg-black grid justify-center text-center items-center">
+      <div data-aos="fade-up">
+        <h1 className="text-white text-7xl">Unitech</h1>
+
+        <p className="text-white text-2xl">Coming soon...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Landng;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,4 @@
-
+import Landng from "@/app/components/landing/index.client";
 export default function Home() {
-  return (
-    <div className="w-full h-screen bg-black grid justify-center text-center items-center">
-      <div data-aos="fade-up">
-      <h1  className="text-white text-7xl">Unitech</h1>
-
-      <p  className="text-white text-2xl">Coming soon...</p>
-
-      </div>
-    </div>
-  );
+  return <Landng />;
 }
