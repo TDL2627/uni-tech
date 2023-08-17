b73c69498000f93fbbff14fbc981b4942734f421
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-08-17 12:12:27 +0200

    update

--- src/app/components/HowTo/index.jsx
+++ src/app/components/HowTo/index.jsx
@@ -1,3 +1,30 @@
+const TimelineBlock = ({ step, content }) => {
+  return (
+    <div className="relative flex items-start">
+      <div className="flex-shrink-0 w-10 h-10 mt-1 bg-black rounded-full flex justify-center items-center">
+        <svg
+          className="w-6 h-6 mx-auto text-white"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M5 13l4 4L19 7"
+          />
+        </svg>
+      </div>
+      <div className="ml-4">
+        <h3 className="font-bold">{step}</h3>
+        <p>{content}</p>
+      </div>
+    </div>
+  );
+};
+
 const HowToRentAFridge = () => {
   const instructions = [
     {
@@ -23,22 +50,24 @@ const HowToRentAFridge = () => {
       content:
         "Be present to receive and install the fridge after agent approval.",
     },
-    // ... (remaining steps, if any)
   ];
   return (
-    <div id="howTo" className="p-6 text-start bg-white ">
-      <h2 data-aos="fade-up" className="text-3xl font-bold mb-6 uppercase  text-center">How to Rent a Fridge</h2>
-      <ul className="list-none  mx-20 space-y-3">
+    <div id="howTo" className="p-6 text-start bg-white">
+      <h2
+        data-aos="fade-up"
+        className="text-3xl font-bold mb-6 uppercase text-center"
+      >
+        How to Rent a Fridge
+      </h2>
+      <div className="mx-20 space-y-6">
         {instructions.map((instruction, index) => (
-          <li key={index} className="mb-3">
-            <p>
-              <span className="font-bold">{instruction.step}</span>
-              <br />
-              {instruction.content}
-            </p>
-          </li>
+          <TimelineBlock
+            key={index}
+            step={instruction.step}
+            content={instruction.content}
+          />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
