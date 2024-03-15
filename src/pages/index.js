0bff850700faf70e853ad1967748b9c3775a2d21
TDL2627 <the.dark.lord.2627@gmail.com>
2024-03-15 11:28:23 +0200

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import HowToRentAFridge from "../app/components/HowTo/index";
 import Landng from "../app/components/landing/index.client";
-import About from "../app/components/pages/Contact";
+import About from "../app/components/pages/About";
 import Products from "../app/components/pages/Products";
 import Testimonials from "../app/components/pages/Testimonials";
 import ContactForm from "../app/components/pages/Contact";
